f764042ad55afcca00eab28647444f9b4139c8ca
Khizer Ali <khizer358@gmail.com>
2023-08-11 16:12:38 +0500

    adding modal for edit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import Modal from './components/Modal'
 function App() {
-  const initialArray = [
-    {
-      id: 1,
-      name: "khizer",
-      age: 23,
-    },
-    {
-      id: 2,
-      name: "Ali",
-      age: 32,
-    },
-    {
-      id: 3,
-      name: "Haider",
-      age: 12,
-    },
-  ];
-  const [addNew, setAddNew] = useState(initialArray);
+  // const initialArray = [
+  //   {
+  //     id: 1,
+  //     name: "khizer",
+  //     age: 23,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Ali",
+  //     age: 32,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Haider",
+  //     age: 12,
+  //   },
+  // ];
+  const [addNew, setAddNew] = useState(JSON.parse(localStorage.getItem('user')));
   const [addName, setaddName] = useState("");
-  const [addAge, setaddAge] = useState("");
+  // const [addAge, setaddAge] = useState("");
   const [blue, setBlue] = useState('')
   const [green, setGreen] = useState('')
 
   const addNewHandler = () => {
-    debugger
+    console.log(addNew, "addNew")
     setAddNew((prevArr) => [
       ...prevArr,
       {
         id: new Date(),
         name: addName,
-        age: addAge,
+        // age: addAge,
       },
-      localStorage.setItem('user', JSON.stringify(prevArr))
     ]);
-    
-    
   };
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(addNew))
+  }, [addNew])
 
   const deleteHandler = (id) => {
     console.log(id);
@@ -46,16 +47,21 @@ function App() {
     setAddNew(updatedArray);
   };
 
+  const editHandler = (userObj) => {
+    <Modal />
+    console.log(userObj)
+    
+  }
+
   const nameHandler = (e) => {
     let text = e.target.value
-    console.log(text);
     setaddName(text)
   }
 
-  const ageHandler = (e) => {
-    let age = e.target.value
-    setaddAge(age)
-  }
+  // const ageHandler = (e) => {
+  //   let age = e.target.value
+  //   setaddAge(age)
+  // }
 
   const blueColorHandler = (id) => {
     setBlue(id)
@@ -66,18 +72,22 @@ function App() {
 
   return (
     <div className="App">
-      <input className="inputText" type="text" name="name" id="name" placeholder="Enter Your Name" value={addName} onChange={nameHandler}/>
-      <input className="inputText" type="number" name="age" id="age" placeholder="Enter Your Age" value={addAge} onChange={ageHandler}/>
+      <h1>TODOLIST !</h1>
+      <input className="inputText" type="text" name="name" id="name" placeholder="Add Your TODO ...." value={addName} onChange={nameHandler}/>
+      {/* <input className="inputText" type="number" name="age" id="age" placeholder="Enter Your Age" value={addAge} onChange={ageHandler}/> */}
       <button style={{marginBottom: '10px'}} className="addBtn" onClick={addNewHandler}>
         Add New
       </button>
       
       <div className="container">
-        {addNew.map((x) => (
+        {addNew?.map((x) => (
           <h1 key={x.id} style={{backgroundColor: blue === x.id ? "blue" : '' || green === x.id ? "green" : ''}}>
             {x.name} <br /> {x.age} <br />
             <button className="delBtn" onClick={() => deleteHandler(x.id)}>
               Delete
+            </button>
+            <button className="delBtn" onClick={() => editHandler(x)}>
+              Edit
             </button>
             <button className="delBtn" onClick={() => blueColorHandler(x.id)}>
               Blue
